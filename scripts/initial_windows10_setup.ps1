3324678b87b68ac795b93daf280fb8dd88147d26
arturhgca <arturhgca@gmail.com>
2018-11-25 11:12:06 -0200

    Add Win10 setup script

---
+++ scripts/initial_windows10_setup.ps1
@@ -0,0 +1,9 @@
+# prepare automation environment
+.\scoop\scoop_setup.ps1
+
+# set Windows properties and remove Windows bloatware
+# TODO: see https://github.com/Disassembler0/Win10-Initial-Setup-Script
+
+# install software
+.\scoop\common_tools.ps1
+.\scoop\dev_tools.ps1
